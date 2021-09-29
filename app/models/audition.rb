2dd76cec94146a6e25f29f3cb781a4e6a457266d
Trestin <trestinishak@gmail.com>
2021-09-29 10:33:39 -0500

    finished

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,3 +1,12 @@
 class Audition < ActiveRecord::Base
-  
+    belongs_to :roles
+
+    def role
+        role = Role.all.find {|role| role.id === self.role_id}
+        role.character_name
+    end
+
+    def call_back
+        self.update(hired: true)
+    end
 end
